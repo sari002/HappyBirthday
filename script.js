076e44f440a763e4aaa3b19c0b142267854de6b3
sari <nazime.sari002@stud.fh-dortmund.de>
2024-03-27 17:53:03 +0100

    added confetti

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.querySelector('#audio audio');
+    if(audio) {
+        audio.addEventListener('play', function() {
+            for(let i = 0; i < 6; i++) {
+                setTimeout(confetti, i * 3000);
+            }
+        });
+    } else {
+        console.log('Audio-Element wurde nicht gefunden');
+    }
+});
