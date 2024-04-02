67ae1bc903dac35ac10e958fcf8f24904f0caaa8
sari <nazime.sari002@stud.fh-dortmund.de>
2024-04-02 04:31:13 +0200

    finished

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const audio = document.querySelector('#audio audio');
     if(audio) {
         audio.addEventListener('play', function() {
-            for(let i = 0; i < 6; i++) {
+            for(let i = 0; i < 9; i++) {
                 setTimeout(confetti, i * 3000);
             }
         });
